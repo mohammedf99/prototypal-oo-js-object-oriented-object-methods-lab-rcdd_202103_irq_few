603169243758f8229ef7e4ba475d556b07cde83b
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 23:16:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ BoardMember.prototype.doCharity = () => "I like to help people.";
 BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber.";
 
 BoardMember.prototype.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+
+
+let mohammed = new BoardMember("Mohammed", "Sulaymaniyah", "Flatiron School");
